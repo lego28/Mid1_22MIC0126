9d296bf7f8d7c910b3aa2e5b59b23e9967b808a0
saket arcot <145363036+lego28@users.noreply.github.com>
2026-03-30 16:28:46 +0530

    fixed some issues

--- Mid.java
+++ Mid.java
@@ -1,8 +1,7 @@
-public class Mid{
+public class Mid {
     public static void main(String[] args) {
-        System.out.println(A D Saket);
-        System.out.println(22MIC0126);
-        System.out.println("Message: Jenkins Build Successful");
-
+        System.out.println("A D Saket");
+        System.out.println("22MIC0126");
+        System.out.println("Jenkins Build Successful");
     }
 }
